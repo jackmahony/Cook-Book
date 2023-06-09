5f42f591df59498cfb168571ec3ade9f2e2ff67d
Jack Mahony <jackmahonyy@gmail.com>
2023-06-09 12:58:54 +1000

    Added native search delay and empty states

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,10 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  connect() {
-    console.log("Testing controller");
-  }
-  submit() {
-    this.element.requestSubmit();
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(()=> {
+      this.element.requestSubmit();
+    }, 1000)
   }
 }
