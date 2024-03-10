847db4e51dae378d89d8d3f737494a582298c2d4
Jack Mahony <jackmahonyy@gmail.com>
2024-03-10 13:12:09 +1000

    Added Flowbite JS and new navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'flowbite'
 
 document.getElementById("copy-to-clipboard").addEventListener("click", function() {
     const copyableText = window.location.pathname;
